8524b6f9626f32425e8409a29e3b20464d81000e
yaroslawbagriy <yaroslawbagriy@gmail.com>
2019-04-30 20:04:21 -0500

    Added ErrorBoundary to catch errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -65,12 +66,14 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             // Always add a key property, allows dom to update onyl what it needs
-            return <Person 
+            return <ErrorBoundary key={person.id}>
+              <Person 
               name={person.name}
               age={person.age}
               key={person.id}
               click={() => this.deletePersonsHandler(index)}
               changed={(event) => this.nameChangedHandler(event, person.id)} />
+            </ErrorBoundary>
           })}
           </div>
       );
